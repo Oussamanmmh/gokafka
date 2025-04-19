f390aa3fc0291a8793ac2bf04fb18d35ebc5a66f
oussamanmh <mo_nemamcha@esi.dz>
2025-04-19 12:36:40 +0200

    make consumer

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 
 func main() {
 
+	topic := "HVSE" //name of the kafka topic
+
 	/*Create a new producer instance
 	with the specified configuration
 	"bootstrap.servers" specifies the Kafka broker address
@@ -27,13 +29,34 @@ func main() {
 		fmt.Printf("Failed to create producer: %s\n", err)
 		os.Exit(1)
 	}
+	go func() {
+		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers":  "localhost:9092",
+			"group.id":           "myGroup",
+			"auto.offset,reset":  "smallest",
+			"enable.auto.commit": "yes",
+			"security.protocol":  "plaintext",
+		})
+		err = consumer.Subscribe(topic, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for {
+			ev := consumer.Poll(100) // 100ms timeout for polling events
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("Received message: %s\n", string(e.Value))
+			case kafka.Error:
+				fmt.Printf("Error: %v\n", e)
 
+			}
+		}
+	}()
 	/*create a channel to handle delivery reports
 	"deleveryChan" is a channel of type kafka.Event with a buffer size of 10000
 	"topic" is the name of the Kafka topic to which messages will be sent*/
 
 	deleveryChan := make(chan kafka.Event, 10000)
-	topic := "HVSE" //name of the kafka topic
 
 	/*produce a message to the specified topic
 	"p.Produce" sends the message to the Kafka broker
@@ -48,6 +71,5 @@ func main() {
 	e := <-deleveryChan
 
 	fmt.Printf("%+v", e)
-	fmt.Printf("%+v", p)
 
 }
